c2e8c25c9a080f3662321b111dbc6ce5545687ec
ilySusu <1303026439@qq.com>
2023-06-02 16:16:27 +0800

    城市按钮滚动的实现

--- src/base-ui/scroll-view/index.jsx
+++ src/base-ui/scroll-view/index.jsx
@@ -2,15 +2,18 @@ import React, {useEffect, useRef, useState} from 'react';
 import PropTypes from 'prop-types';
 import {ViewWrapper} from "@/base-ui/scroll-view/style";
 import {Button} from "antd";
+import IconArrowLeft from "@/assets/svg/icon-arrow-left";
+import IconArrowRight from "@/assets/svg/icon-arrow-right";
 
 // 封装 点击 左右按钮 滚动视图的组件
 const ScrollView = props => {
   // 定义内部的状态
+  const [showLeftBtn, setShowLeftBtn] = useState(false)
   const [showRightBtn, setShowRightBtn] = useState(false)
   const [posIndex, setPosIndex] = useState(0)
 
-  // 记录总距离
-  const totalDistanceRef = useRef()
+  // 记录组件重新渲染 不可变的值
+  const totalDistanceRef = useRef() // 总距离
 
 
   // 组件渲染完毕，判断是否显示右侧按钮
@@ -23,28 +26,42 @@ const ScrollView = props => {
     setShowRightBtn(totalDistance > 0)
   }, [props.children])
 
-  // 事件处理逻辑
-  const handleRightClick = () => {
-    const newIndex = posIndex + 1
-    // 拿到下一个城市按钮的item
+  // 点击按钮的事件处理逻辑
+  const handleBtnClick = (isRight) => {
+    // 获取下一个要偏移元素的索引
+    const newIndex = isRight ? posIndex + 1 : posIndex - 1
+    // 获取下一个城市按钮的item
     const newEl = contentRef.current.children[newIndex]
-    // 拿到向左测可以偏移的距离
-    // console.log(newEl.offsetLeft)
-    contentRef.current.style.transform = `translate(-${newEl.offsetLeft}px)`
+    // 获取向左测可以偏移的距离
+    console.log(newEl.offsetLeft)
+    const newOffsetLeft = newEl.offsetLeft
+    contentRef.current.style.transform = `translate(-${newOffsetLeft}px)`
     // 设置最新的索引
     setPosIndex(newIndex)
     // 是否继续显示右侧按钮
-    setShowRightBtn(totalDistanceRef.current > newEl.offsetLeft)
+    setShowRightBtn(totalDistanceRef.current > newOffsetLeft)
+    // 左侧偏移量大于0， 显示左侧按钮
+    setShowLeftBtn(newOffsetLeft > 0)
   }
 
   return (
     <ViewWrapper>
-      <Button>left btn</Button>
-      {showRightBtn && <Button onClick={handleRightClick}>right btn</Button>}
+      {showLeftBtn && (
+        <div className="btn left" onClick={e => handleBtnClick(false)}>
+          <IconArrowLeft />
+        </div>
+      )}
+      {showRightBtn && (
+        <div className="btn right" onClick={e => handleBtnClick(true)}>
+          <IconArrowRight />
+        </div>
+      )}
 
-      {/* 中间的内容是不确定的，预留一个插槽 外面传递 */}
-      <div className="content" ref={contentRef}>
-        {props.children}
+      <div className="scroll">
+        {/* 中间的内容是不确定的，预留一个插槽 外面传递 */}
+        <div className="content" ref={contentRef}>
+          {props.children}
+        </div>
       </div>
     </ViewWrapper>
   );
--- src/base-ui/scroll-view/style.js
+++ src/base-ui/scroll-view/style.js
@@ -1,11 +1,39 @@
 import styled from "styled-components";
 
 export const ViewWrapper = styled.div`
-  overflow: hidden;
-  .content {
-    position: relative;
+  // offsetLeft偏移量，需要加定位， 不然以body等元素为边界
+  position: relative;
+  .scroll {
+    overflow: hidden;
+    .content {
+      display: flex;
+      transition: all .2s ease;
+    }
+  }
+  
+  .btn {
+    position: absolute;
     display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    border: 1px solid #ccc;
+    background-color: #fff;
+    cursor: pointer;
+    z-index: 10;
     
-    transition: all .2s ease;
+    &.left {
+      left: 0;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    &.right {
+      right: 0;
+      top: 50%;
+      transform: translate(50%, -50%);
+    }
   }
 `
